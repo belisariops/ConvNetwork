corrected backpropagation

ConvolutionalLayer.py
@@ -8,7 +8,7 @@ class ConvolutionalLayer(NeuralLayer):
     def __init__(self,kernelSize,numKernels):
         super().__init__()
         for i in range(numKernels):
-            self.kernels.append(np.random.random((kernelSize,kernelSize)))
+            self.kernels.append(np.random.random((kernelSize,kernelSize))*4 - 1)
 
 
     def forwardPropagation(self,inputs):
@@ -36,14 +36,23 @@ class ConvolutionalLayer(NeuralLayer):
         self.nextLayer.forwardPropagation(self.outputFeatureMap)
 
     def updateWeights(self,inputs):
+        # Revisar caso en que la imagen es de un canal (blanco y negro)
+        try:
+            channels = inputs.shape[2]
+        except IndexError:
+            channels = 1
         for index,delta in enumerate(self.deltas):
-            for input in inputs:
-                self.kernels[index] = np.add(self.kernels[index],signal.convolve2d(input,np.rot90(delta,2),'valid'))
+            for channel in range(channels):
+                self.kernels[index] = np.add(self.kernels[index],signal.convolve2d(inputs[:,:,channel],np.rot90(delta,2),'valid'))
 
 
     def backPropagation(self):
-        for kIndex,kernel in enumerate(self.kernels):
-            for dIndex in range(len(self.previousLayer.deltas)):
-                self.previousLayer.deltas[dIndex] = np.add(self.previousLayer.deltas[dIndex],signal.convolve2d(np.rot90(kernel,2),self.deltas[kIndex],'full'))
-
+        try:
+            for kIndex,kernel in enumerate(self.kernels):
+                for dIndex in range(len(self.previousLayer.deltas)):
+                    self.previousLayer.deltas[dIndex] = np.add(self.previousLayer.deltas[dIndex],signal.convolve2d(np.rot90(kernel,2),self.deltas[kIndex],'full'))
+        #Case that previous layer is an InputLayer
+        except AttributeError:
+            self.previousLayer.backPropagation()
+        self.previousLayer.backPropagation()
 

FlattenLayer.py
@@ -9,18 +9,19 @@ class FlattenLayer(AbstractNeuralLayer):
         self.thisShape = None
         self.deltas = []
         self.outputs = []
+        self.myInput = None
 
 
     def buildNeurons(self,numNeurons):
         self.buildRandomLayer(numNeurons)
-        self.setLearningRate(0.01)
+        # self.setLearningRate(0.01)
         self.isBuilded = True
         self.setRandomWeights(1,-2,2)
 
     def forwardPropagation(self,input):
         self.thisShape = input.shape
         # Aplanar el input primero y usar una capa de perceptron multiple
-        myInput = input.flatten()
+        self.myInput = input.flatten()
         # Revisar caso en que la imagen es de un canal (blanco y negro)
         try:
             channels = input.shape[2]
@@ -31,13 +32,15 @@ class FlattenLayer(AbstractNeuralLayer):
         self.deltas = []
         for channel in range(channels):
             self.deltas.append(np.zeros((inputHeight,inputWidth)))
-
         if (not self.isBuilded):
-            self.buildNeurons(len(myInput))
+            self.buildNeurons(len(self.myInput))
+        for neuron in self.neuron_array:
+            if len(neuron.weights) > 1:
+                print("aaaaaaaaaaaa")
         self.outputs = []
         for index,neuron in enumerate(self.neuron_array):
-            neuron.setInputsList(myInput[index])
-            neuron.output = neuron.getOutput([myInput[index]])
+            neuron.setInputsList(self.myInput[index])
+            neuron.output = neuron.getOutput([self.myInput[index]])
             self.outputs.append(neuron.output)
         return self.next_layer.forwardPropagation(self.outputs)
 
@@ -48,4 +51,12 @@ class FlattenLayer(AbstractNeuralLayer):
             self.deltas[index] = neuron.delta
         self.previousLayer.deltas = np.reshape(self.deltas,self.thisShape)
         self.previousLayer.backPropagation()
+
+    def applyPropagationChanges(self,inputs):
+        outputs = []
+        for index,neuron in enumerate(self.neuron_array):
+            neuron.updateWeightEscalarInput(self.myInput[index])
+            neuron.updateBias()
+            outputs.append(neuron.output)
+        self.next_layer.applyPropagationChanges(outputs)
 

PoolingLayer.py
@@ -53,7 +53,7 @@ class PoolingLayer(NeuralLayer):
                     h =int(h)
                     w = int(w)
                     self.previousLayer.deltas[channel][h,w] = self.deltas[dh,dw,channel]
-            self.previousLayer.deltas = np.multiply(self.previousLayer.deltas[channel],self.markNeurons)
+            # self.previousLayer.deltas[channel] = np.multiply(self.previousLayer.deltas[channel],self.markNeurons)
 
         self.previousLayer.backPropagation()
 